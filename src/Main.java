7a5396ae973b7ebeb3be206390ef11d5b63f0462
Bastian Stade <bastian.stade@stud.fra-uas.de>
2023-01-10 17:01:42 +0100

    changed "halfangle" to "delta" fixed sleep duration after each shape from sleep the wrong variable to the right one

--- src/Main.java
+++ src/Main.java
@@ -97,7 +97,7 @@ public class Main {
         }
         int width = panel.getWidth();
         int height = panel.getHeight();
-        int endX = width / 2, endY = height - length, halfAngle = (int)angle / 2;
+        int endX = width / 2, endY = height - length, delta = (int)angle / 2;
         //sP = startPoint | x of sP is always in the middle of the panel and y of sP is always at the bottom of the Panel
         Point eP, sP = new Point(width / 2, height);
         ArrayList<Point> currStartPoints = new ArrayList<Point>();
@@ -128,14 +128,14 @@ public class Main {
                 for(int i = 0; i <= currStartPoints.size() - 1; i++){
                     sP = currStartPoints.get(i);
                     for(int j = 0; j < 2; j++){
-                        endX = (int)(sP.getX() + length * Math.sin(Math.PI * (currAngles.get(i) + halfAngle * Math.pow(-1, j)) / 180));
-                        endY = (int)(sP.getY() - length * Math.cos(Math.PI * (currAngles.get(i) + halfAngle * Math.pow(-1, j)) / 180));
+                        endX = (int)(sP.getX() + length * Math.sin(Math.PI * (currAngles.get(i) + delta * Math.pow(-1, j)) / 180));
+                        endY = (int)(sP.getY() - length * Math.cos(Math.PI * (currAngles.get(i) + delta * Math.pow(-1, j)) / 180));
                         g2d.drawLine(sP.getX(), sP.getY(), endX, endY);
                         nextStartPoints.add(new Point(endX, endY));
-                        nextAngles.add((int)(currAngles.get(i) + halfAngle * Math.pow(-1, j)));
+                        nextAngles.add((int)(currAngles.get(i) + delta * Math.pow(-1, j)));
                         if(sleepAfterShape){
                             try {
-                                Thread.sleep(sleepAfterIterMS);
+                                Thread.sleep(sleepAfterShapeMS);
                             } catch (InterruptedException e) {
                                 // TODO Auto-generated catch block
                                 e.printStackTrace();
